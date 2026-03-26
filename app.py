9d443151c8a27cabf4e443b78d3fd8978701f1ef
JavierMauricioLlanos <javierllanosatoy@gmail.com>
2026-03-25 20:08:26 -0500

    Actualizo archivos

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 # TODO: desarrollar el sistema
 
+class Hotel :
+    def __init__(self, nombre, direccion, telefono, email, coordenadas, servicios, Estado):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.email = email
+        self.coordenadas = coordenadas
+        self.servicios = servicios
+        self.Estado = Estado
+        
+class Habitacion :
+    def __init__(self, numero, tipo, precio, disponibilidad):
+        self.numero = numero
+        self.tipo = tipo
+        self.precio = precio
+        self.disponibilidad = disponibilidad
+        
